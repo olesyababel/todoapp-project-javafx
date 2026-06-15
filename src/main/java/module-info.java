d5ea6e210ad985954f6493a23f3d0278edbe6299
mariaprang <mprangishvili@newwork.com>
2026-06-15 19:35:39 +0400

    changed maven version

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.todoappjavafx {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.todoappjavafx to javafx.fxml;
-    exports com.example.todoappjavafx;
-}
