b7973f664e505e39233978d341ddc4ff72116d01
mariaprang <mprangishvili@newwork.com>
2026-06-17 10:47:51 +0400

    added two buttons with stage views

--- src/main/java/com/example/todoappjavafx/HelloController.java
+++ src/main/java/com/example/todoappjavafx/HelloController.java
@@ -1,22 +1,45 @@
 package com.example.todoappjavafx;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
 
 public class HelloController {
+
     @FXML
     private Label welcomeText;
 
-    //hello again
-    // second commit
-
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        Stage stage = new Stage();
+
+        VBox vBox = new VBox();
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setSpacing(20);
+        vBox.getChildren().add(new Label("New window"));
+        Scene scene = new Scene(vBox, 300,300);
+        stage.setScene(scene);
+
+        stage.show();
     }
 
     @FXML
     public void onByeButtonClick(){
-        welcomeText.setText("Bye Bye!");
+
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.setTitle("Question");
+
+        DialogPane dialogPane = dialog.getDialogPane();
+        dialogPane.setHeaderText("A new software update is ready.");
+        dialogPane.setContentText("Would you like to install it and restart now?");
+        dialogPane.getButtonTypes().addAll(ButtonType.YES, ButtonType.NO);
+
+        dialog.showAndWait();
     }
 }
